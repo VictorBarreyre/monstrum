da82b6c3ad6276b4f4def9506f8dafc8233aa7c1
VictorBarreyre <barreyrevictor.contact@gmail.com>
2024-01-06 12:01:00 +0100

    first-commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div className='fullnav'>
+      <div className='navtop'>
+      <a className='topa'> Jeunecrouteur</a>
+      <a className='topa'> Contact</a>
+      </div>
+      <div className='navbottom'>
+      <div className='leftflexbottom'>
+      <a className='topa'> Float</a>
+      <a className='topa'> Grid</a>
+      <a className='topa'> List</a>
+      </div>
+     <span className='topa'>All rights reserved ©Jeunecrouter </span>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
